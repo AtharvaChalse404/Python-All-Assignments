38c87a44ecca1a32bd8302bdc39232f095ed230b
Atharva <143740199+AtharvaChalse404@users.noreply.github.com>
2024-02-14 01:37:41 +0530

    .

---
+++ new/nested loop.py
@@ -0,0 +1,4 @@
+for i in range(1, 6):  # Outer loop for the number of rows
+    for j in range(i):  # Inner loop for printing the numbers
+        print(i, end="")
+    print()  # Move to the next line after each row
